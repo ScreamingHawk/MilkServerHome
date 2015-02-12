14f1f4a44275b0c495563063758fc19b8f5ddf48
Michael Standen <screaminghawk@gmail.com>
2015-02-12 13:33:17 +1300

    Updated build script to add commitid and fixed buildhead

--- buildhead.bat
+++ buildhead.bat
@@ -1,3 +1,5 @@
 rem This script builds from the HEAD not from the local machine
 del /f build.zip
-git archive --format=zip HEAD > build.zip
+for /f %%i in ('git rev-parse HEAD') do set commitId=%%i
+git archive --format=zip HEAD > build_%commitId%.zip
+
