f0e2675eafb17789acc215ebf678b0861538889e
Michael Standen <screaminghawk@gmail.com>
2015-02-13 10:55:12 +1300

    Updated build scripts to remove previous builds before building

--- buildhead.bat
+++ buildhead.bat
@@ -1,5 +1,5 @@
 rem This script builds from the HEAD not from the local machine
-del /f build.zip
+del /f build*.zip
 for /f %%i in ('git rev-parse HEAD') do set commitId=%%i
 git archive --format=zip HEAD > build_%commitId%.zip
 
--- buildlocal.bat
+++ buildlocal.bat
@@ -1,3 +1,3 @@
 rem This script builds from the HEAD not from the local machine
-del /f build.zip
+del /f build*.zip
 7z a -tzip build.zip *
