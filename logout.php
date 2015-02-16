7bfa292bb1eed3f18ce8f6ba5ec0be106f243bc3
Michael Standen <screaminghawk@gmail.com>
2015-02-16 13:50:32 +1300

    Added logout script

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'includes/functions.php';
+
+sec_session_start();
+$_SESSION = array();
+$params = session_get_cookie_params();
+setcookie(session_name(), '', 
+		time() - 42000, 
+		$param["path"], 
+		$params["domain"], 
+		$params["secure"], 
+		$params["httponly"]);
+
+session_destroy();
+header('Location: /');
