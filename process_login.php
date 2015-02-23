9bfd98fe9653ac8fed2eb59a47f8696550fc1dd1
Michael Standen <screaminghawk@gmail.com>
2015-02-23 14:56:41 +1300

    Fixed file linking

--- process_login.php
+++ process_login.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/includes/db-connect.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/includes/db_connect.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/includes/functions.php';
 sec_session_start();
 if (isset($_POST['email'], $_POST['p'])) {
